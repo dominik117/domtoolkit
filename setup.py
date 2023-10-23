3c4d85ef6469cc7dfa756e2706bfce6901492e3b
Dominik Bacher <dominikbacher@icloud.com>
2023-10-23 11:37:24 +0200

    Package needed files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='domtoolkit',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Toolkit for general Data Science operations',
+    author='Dominik Bacher',
+    license='MIT',
+)
